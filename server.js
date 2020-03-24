2d4498bc340bd541a484ba0d7a865ec80e3f42be
Julian Vazquez <julianvazquez101@gmail.com>
2020-03-23 20:12:27 -0400

    Prepare for first deployment

--- server.js
+++ server.js
@@ -7,8 +7,14 @@ const movies = require('./routes/movies');
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(`${__dirname}/client/build`));
 
+// Routes
 app.use('/movies', movies);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 const PORT = 8000;
 
