831a94a685e1156328071dee2c0afa717367e769
Julian Vazquez <julianvazquez101@gmail.com>
2020-03-26 13:52:14 -0400

    Fix spacing

--- client/src/common/Watchlist.js
+++ client/src/common/Watchlist.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { FullWidthContainer } from '../styles/styled-components';
+import styled from 'styled-components';
 import WatchlistGrid from './WatchlistGrid';
+
+const FullWidthWatchlistContainer = styled(FullWidthContainer)`
+  padding: 5rem 0.5rem;
+
+  @media (min-width: 600px) {
+    padding: 5rem 2rem;
+  }
+`;
 
 const Home = () => {
   return (
-    <FullWidthContainer>
+    <FullWidthWatchlistContainer>
       <WatchlistGrid />
-    </FullWidthContainer>
+    </FullWidthWatchlistContainer>
   );
 };
 
